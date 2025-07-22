7bf64be683231545213d79fb712cd91bb7286673
Fuyad Hasan Fahim <codewithfuyad@gmail.com>
2025-07-22 15:35:26 +0600

    trying to fix the paylater option.

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,19 @@ import { verifyAuth } from "./middleware/verifyAuth.js";
 import cookieParser from "cookie-parser";
 import StripeControllers from "./controllers/stripe.controller.js";
 import envConfig from "./config/env.config.js";
-import './jobs/chargeOverduePayments';
+import "./jobs/chargeOverduePayments";
 
 const { frontend_url } = envConfig;
 
 const app: Application = express();
+
+const corsOptions = {
+    origin: frontend_url!,
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions));
 
 app.post(
     "/api/stripe/payment-webhook",
@@ -18,13 +26,7 @@ app.post(
     StripeControllers.paymentWebhook
 );
 
-const corsOptions = {
-    origin: frontend_url!,
-    credentials: true,
-};
-
 app.use(express.json());
-app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
